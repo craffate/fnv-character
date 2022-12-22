fe47a3d3627d2b2a023b85efc27815da87b66606
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2022-12-22 09:31:58 +0100

    Add simple tests

--- src/app/services/character.service.spec.ts
+++ src/app/services/character.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { Character } from '@classes/character';
 
 import { CharacterService } from './character.service';
+import { DEFAULT_CHARACTER } from './default-character';
 
 describe('CharacterService', () => {
   let service: CharacterService;
+  let character: Character;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,4 +16,11 @@ describe('CharacterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return DEFAULT_CHARACTER', () => {
+    service.getCharacter().subscribe(v => {
+      character = v;
+      expect(v).toEqual(DEFAULT_CHARACTER);
+    });
+  })
 });
